6d4997d8c8cd871b4f80975c98a4deca85835bd3
Camilo <camilo.mh2014@gmail.com>
2025-02-26 16:55:53 -0300

    feat: enum de modos de autorización

--- src/main/java/com/redpay/enums/AuthorizationMode.java
+++ src/main/java/com/redpay/enums/AuthorizationMode.java
@@ -1,5 +1,10 @@
 package com.redpay.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+import java.util.Locale;
+
 /**
  * Enum que representa los diferentes modos de operación de una devolución.
  * Utilizado para definir el propósito de la operación en servicios de RedPay.
@@ -9,20 +14,35 @@ package com.redpay.enums;
  * - Chargeback_Automatic: Representa una devolución automática de transacción. <br>
  * </p>
  */
+@Getter
 public enum AuthorizationMode {
     Authorize("authorize"),
-
     Chargeback("chargeback"),
-
     Chargeback_Automatic("chargeback_automatic");
 
     private final String authorize;
 
+    /**
+     * Constructor que inicializa el modo de autorización con su representación en String.
+     *
+     * @param authorize Valor que representa el modo de autorización.
+     */
     AuthorizationMode(String authorize) {
         this.authorize = authorize;
     }
 
-    public String getAuthorize() {
-        return authorize;
+    /**
+     * Devuelve el valor del modo de autorización en minúsculas.
+     * <p>
+     * Este método se utiliza para la serialización a JSON, asegurando que
+     * el valor devuelto sea siempre en minúsculas.
+     * </p>
+     *
+     * @return El modo de autorización en minúsculas.
+     */
+    @JsonValue
+    public String getCode() {
+        return authorize.toLowerCase(Locale.ROOT);
     }
+
 }
