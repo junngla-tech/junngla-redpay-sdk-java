8ac4f079f7fecb3393a8b977106a7ca648001c59
Camilo <camilo.mh2014@gmail.com>
2025-02-26 16:56:21 -0300

    feat: enum para roles de enrolador

--- src/main/java/com/redpay/enums/Enroller.java
+++ src/main/java/com/redpay/enums/Enroller.java
@@ -1,9 +1,12 @@
 package com.redpay.enums;
 
+import lombok.Getter;
+
 /**
  * Enum que representa los tipos de roles que puede tener un enrolador.
  * Cada valor es un rol que puede tener un enrolador.
  */
+@Getter
 public enum Enroller {
     COLLECTOR("collector"),
     PAYER("payer"),
@@ -11,11 +14,12 @@ public enum Enroller {
 
     private final String role;
 
+    /**
+     * Constructor que asigna el valor del rol al enrolador.
+     *
+     * @param role Valor que representa el rol del enrolador.
+     */
     Enroller(String role) {
         this.role = role;
-    }
-
-    public String getRole() {
-        return role;
     }
 }
