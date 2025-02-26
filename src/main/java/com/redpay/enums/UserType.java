5fc0d25d353e6334bc2ddbeb9f552c8de6a9e5b2
Camilo <camilo.mh2014@gmail.com>
2025-02-26 16:58:43 -0300

    feat: enum con los tipos de usuario

--- src/main/java/com/redpay/enums/UserType.java
+++ src/main/java/com/redpay/enums/UserType.java
@@ -1,8 +1,14 @@
 package com.redpay.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+import java.util.Locale;
+
 /**
  * Enum que define los tipos de usuarios en el sistema.
  */
+@Getter
 public enum UserType {
 
     COLLECTOR("collector"),
@@ -10,11 +16,27 @@ public enum UserType {
 
     private final String value;
 
+    /**
+     * Constructor que asigna el valor asociado al tipo de usuario.
+     *
+     * @param value Valor que representa el tipo de usuario.
+     */
     UserType(String value) {
         this.value = value;
     }
 
-    public String getValue() {
-        return value;
+    /**
+     * Devuelve el valor en minúsculas que representa el tipo de usuario.
+     * <p>
+     * Este método se utiliza para la serialización a JSON, garantizando que el valor
+     * se represente en minúsculas.
+     * </p>
+     *
+     * @return El valor en minúsculas del tipo de usuario.
+     */
+    @JsonValue
+    public String getCode() {
+        return value.toLowerCase(Locale.ROOT);
     }
+
 }
